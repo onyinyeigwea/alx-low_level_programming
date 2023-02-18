198e47853d736f3a76629e26018472307b8c9b99
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-02-17 16:35:04 -0800

    A piece of art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1
+ *
+ * Return: Always 1 (Sucess)
  */
-
 int main(void)
-
 {
-	char ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
-	putchar(ch);
-	return (0);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
