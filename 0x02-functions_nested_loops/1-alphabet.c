0d033546c547406458e119e2b48a9bb08ea7e3e9
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-02-21 12:53:40 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{_putchar(n);
+	}
+	_putchar('\n');
+}
