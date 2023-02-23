228e3ebef4a759050f8fd97985e81f456e30bffc
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-02-23 11:37:51 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,21 +4,17 @@
  *  * print_most_numbers - checks for checks for a digit (0 through 9).
  *   *
  *    * Return: Always 0.
- *     */
+*/
 void print_most_numbers(void)
 {
-	 int c;
+	int numb;
 
-	  for (c = 48; c < 58; c++)
-		   {
-			    if (c != 50)
-				     {
-					      if (c != 52)
-						       {
-							        _putchar(c);
-								 }
-					       }
-			     }
-	   _putchar('\n');
-1-isdigit.c
+	for (numb = 48 ; numb <= 57 ; numb++)
+	{
+		if (numb != 50 && numb != 52)
+		{
+			_putchar(numb);
+		}
+	}
+	_putchar('\n');
 }
