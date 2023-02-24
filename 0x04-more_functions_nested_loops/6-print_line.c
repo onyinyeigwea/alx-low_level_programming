4d7b10004b3b704e94b299fa624d9a82dc4cab9e
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-02-24 12:27:43 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,18 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *  * print_line - draws a straight line in the terminal
- *   * @n: number of times the character _ should be printed
- *    */
+ * print_line -draws a straight line in the terminal
+ * @n: the number of times that the special character must be printed
+ *
+ * Return: a straight line
+ */
 void print_line(int n)
 {
-	 if (n <= 0)
-		  {
-			   _putchar('\n');
-			    } else
-				     {
-					      int i;
+	int a = 1;
 
-					       for (i = 1; i <= n; i++)
-						        {
-								 _putchar('_');
-								  }
-					        _putchar('\n');
-						 }
-
+	while (a <= n)
+	{
+		_putchar(95);
+		a++;
+	}
+	_putchar('\n');
 }
