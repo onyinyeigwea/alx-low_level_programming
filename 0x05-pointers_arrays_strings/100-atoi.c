73e377dc12f1ae61cec44a62c14bf09193142654
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-02-27 09:01:00 -0800

    Converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,6 +7,25 @@
  */
 int _atoi(char *s)
 {
+	int sign;
+	unsigned int num;
+	char *temp;
 
-	return (0);
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
