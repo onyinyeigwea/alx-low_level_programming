959354c5d2e1e34716fa6bb2f89b7035e595e18e
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-03-26 10:25:12 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,36 +8,27 @@
  * @s2: string to add to end of of first string
  *
  * Return: pointer to newly allocated string concatenation
+ *
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int size1 = 0, size2 = 0;
-	char *ptr, *ret;
-
-	ptr = s1;
-	if (s1)
-		while (*ptr++)
-			size1++;
-	else
+	if (s1 == NULL)
 		s1 = "";
-
-	ptr = s2;
-	if (s2)
-		while (*ptr++)
-			size2++;
-	else
+	if (s2 == NULL)
 		s2 = "";
 
-	ret = malloc(size1 + size2 + 1);
-	if (!ret)
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	char *result = malloc(len1 + len2 + 1);
+	if (result == NULL)
+
 		return (NULL);
 
-	ptr = ret;
-	while (*s1)
-		*ptr++ = *s1++;
-	while (*s2)
-		*ptr++ = *s2++;
-	*ptr = 0;
+	strcpy(result, s1);
+	strcpy(result + len1, s2);
 
-	return (ret);
+	return (result);
+
 }
+
