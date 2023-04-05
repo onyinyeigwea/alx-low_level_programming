7953409faa323867bafdd6dbf58c86ce5cfbde8c
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-04-04 21:58:51 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *Z;
+	while (head != NULL)
+	{
+	Z = head;
+	head = head->next;
+	free(Z);
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t *head)
+{
+        listint_t *Z;
+        while (head != NULL)
+        {
+        Z = head;
+        head = head->next;
+        free(Z);
+        }
+}
