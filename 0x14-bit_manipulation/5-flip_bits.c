7470650384d50733f0386c3b5640e0bef8affcb3
onyinyeigwea <onyinyeigwe.a@gmail.com>
2023-04-09 11:50:07 -0700

    Flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of bits to flip
+ * @n: this is the first no
+ * @m:The second no
+ *
+ * Return: the number of bits to flip to get from n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int distinct = n ^ m;
+	unsigned int count = 0;
+
+	while (distinct)
+	{
+		count += distinct & 1;
+	}
+	return (count);
+}
